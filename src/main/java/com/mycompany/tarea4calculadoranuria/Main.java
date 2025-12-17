8898386db5bd0a66651abec2d90a93d11b022bb2
Nuria <nuriadr2212@gmail.com>
2025-12-17 09:37:57 +0100

    Fix JFrame size and add PanelPrincipal

--- src/main/java/com/mycompany/tarea4calculadoranuria/Main.java
+++ src/main/java/com/mycompany/tarea4calculadoranuria/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculadora");
         frame.setBounds(250, 250, 0, 0);
-        frame.add(new PanelPrincipal())
+        frame.add(new PanelPrincipal());
 
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
